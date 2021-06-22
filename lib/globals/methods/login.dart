18da74f53482313a2d66f8499690c59d6b6bd7e3
Patrick Issler <patrickissler1234@gmail.com>
2021-06-22 15:52:24 -0300

    salvando token no dispositivo

--- lib/globals/methods/login.dart
+++ lib/globals/methods/login.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:testemarketeasy/globals/globals.dart';
-import 'package:testemarketeasy/globals/methods/produtos.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Login {
   static Future<String> logar(login, senha) async {
@@ -16,6 +16,13 @@ class Login {
       if(status == "ok"){
         token = dadosjson['response']['token'].toString();
         tokenExpiration = dadosjson['response']['tokenExpiration'];
+       try{
+         SharedPreferences prefs = await SharedPreferences.getInstance();
+         prefs.setString("token", token);
+         prefs.setString("tokenExpiration", tokenExpiration.toString());
+       }catch(e){
+         print("e");
+       }
         return "sucesso";
       }else{
         return "Usuario ou senha incorretos";
