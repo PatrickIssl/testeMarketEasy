769e9c4e1849125ed7e47efb1f4ce2f54d510ce5
Patrick Issler <patrickissler1234@gmail.com>
2021-06-22 11:59:31 -0300

    login concluido

--- lib/globals/methods/login.dart
+++ lib/globals/methods/login.dart
@@ -4,13 +4,25 @@ import 'package:testemarketeasy/globals/globals.dart';
 import 'package:testemarketeasy/globals/methods/produtos.dart';
 
 class Login {
-  static logar(login, senha) async {
+  static Future<String> logar(login, senha) async {
     var corpo = json.encode({"usuario": login, "senha": senha});
 
     http.Response response = await http.post(urlPostLogin,
         headers: {"Content-type": "application/json; charset=UTF-8"},
         body: corpo);
     var dadosjson = json.decode(response.body);
-    token = dadosjson['response']['token'].toString();
+    var status = dadosjson['response']["status"];
+    if(response.statusCode == 200){
+      if(status == "ok"){
+        token = dadosjson['response']['token'].toString();
+        tokenExpiration = dadosjson['response']['tokenExpiration'];
+        return "sucesso";
+      }else{
+        return "Usuario ou senha incorretos";
+      }
+    }else{
+      return "Erro na requisição de login";
+    }
+
   }
 }
